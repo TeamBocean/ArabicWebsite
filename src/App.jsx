de8d1810c473ca474fde0b3630bd2bb520a72ae0
TeamBocean <team.bocean@gmail.com>
2023-09-04 17:52:23 +0100

    added plans buttons

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,19 @@ import Clients from "./components/Clients"
 import Contact from "./components/Contact"
 import Footer from "./components/Footer"
 
-
-
-
 function App() {
+
   return (
     <div className="bg-white min-h-screen overflow-hidden">
-      <Header/>
-      <Hero/>
-      <About/>
-      <Pricing/>
-      <Clients/>
-      <Contact/>
-      <Footer/>
+      <Header />
+      <Hero />
+      <About />
+      <Pricing />
+      <Clients />
+      <Contact />
+      <Footer />
     </div>
   )
 }
 
 export default App
-
