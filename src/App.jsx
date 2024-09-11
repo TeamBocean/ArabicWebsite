870a50eccc39afc4203a3e90d178f3974f602c02
TeamBocean <team.bocean@gmail.com>
2024-09-11 13:58:53 +0100

    deploy to vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import Header from "./components/Header";
 import Pricing from "./components/Pricing";
 import About from "./components/About";
@@ -6,27 +6,12 @@ import Hero from "./components/Hero";
 import Clients from "./components/Clients";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import { analytics } from "./firebase";
-import ReactGA from 'react-ga';
+import { Analytics } from "@vercel/analytics/react"
 
-ReactGA.initialize('6238026937'); // Replace with your Tracking ID
 
-ReactGA.pageview(window.location.pathname + window.location.search);
-
-const usePageViews = () => {
-  let location = useLocation();
-  useEffect(() => {
-    window.gtag('config', 'G-QEXV0B4QH0', {
-      page_path: location.pathname + location.search,
-    });
-  }, [location]);
-};
 
 function App() {
-  usePageViews();
   useEffect(() => {
-    // Log an event when the app loads
-    analytics.logEvent('page_view', { page_title: 'Home Page' });
   }, []);
 
   return (
@@ -38,6 +23,7 @@ function App() {
       <Clients />
       <Contact />
       <Footer />
+      <Analytics/>
     </div>
   );
 }
